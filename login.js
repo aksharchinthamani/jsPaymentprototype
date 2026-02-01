e1d8ed4e9a16ce93cac91bfa7f1b89fb94ccea67
aksharchinthamani <chinthamaniakshar@gmail.com>
2026-02-01 22:06:44 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -18,22 +18,18 @@ function signup() {
 }
 
 function next() {
-  // const username = document.getElementById("username").value.trim();
-  // const password = document.getElementById("password").value.trim();
-  // const error = document.getElementById("error");
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const error = document.getElementById("error");
 
-  // const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedUser = JSON.parse(localStorage.getItem("user"));
 
-  // if (storedUser && username === storedUser.username && password === storedUser.password) {
-  //   window.location.href = "convertion.html"; 
-  // } else {
-  //   error.style.color = "red";
-  //   error.textContent = "Invalid credentials. Please try again.";
-  // }
-  if(password.value !=0 && username.value !=0){
-  window.location.href = "card.html";
-}
-else{
-    alert("please enter your login details")
-}
+  if (storedUser && username === storedUser.username && password === storedUser.password) {
+    window.location.href = "card.html"; 
+  } else {
+    error.style.color = "red";
+    error.textContent = "Invalid credentials. Please try again.";
+  }
+ 
+
 }
